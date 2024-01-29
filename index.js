8bc87f7783775ea4afc87184a4c6ccdd3e270b5f
chedly-k <chedlykhiari5@gmail.com>
2024-01-29 12:09:21 +0100

    doo

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function test (a,b){
-    if (((a<0 && b>0) || (a>0 && b<0))){
-        console.log("negatif")
-    }else{
-        console.log('positive')
-    }
+function tableau (tab,x){
+if(typeof(x)=="number" && x>0){
+    tab.push(x)
+    return tab 
+}else{
+    console.log ("error")
+}
 
-    
+
 }
